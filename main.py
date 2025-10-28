3d18c08d2307ef04db82b0dae0cd312642b6b0ad
badbird <ysihigh74@gmail.com>
2025-10-28 20:34:11 +0800

    add:添加llvm对战绩进行评价功能

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register, StarTools
 from astrbot.api import logger
+from astrbot.api.message_components import Face, Plain, At
+# from astrbot.core.provider import Provider
+
+
 #todo: 增加错误处理和日志记录
 #todo: 优化数据存储结构，支持多个玩家数据，解决self数据的覆盖问题
 #todo：增加战绩数据的格式化输出，而不是仅仅输出地图名称
@@ -22,6 +26,8 @@ class Cstatscheck(Star):
         self.playername: str = "" # user 对应的玩家名称
         self.domain: str = "" # 仅仅用于查询 playername 对应的 uuid
         self.uuid: str = "" # 用于查询战绩
+        self._session = None
+        # self.llvm = Provider()
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
@@ -31,6 +37,7 @@ class Cstatscheck(Star):
         if not self.user_data_file.exists():
             with open(self.user_data_file, "w", encoding="utf-8") as f:
                 json.dump({}, f)
+        # self._session = aiohttp.ClientSession()
 
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(1)) # 重试3次，每次间隔2秒
     async def get_domain(self, session: aiohttp.ClientSession):
@@ -120,7 +127,7 @@ class Cstatscheck(Star):
                 return match_list[0].get("match_id")
             return None
 
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1)) # 重试3次，每次间隔2秒
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1)) # 重试3次，每次间隔1秒
     async def get_match_stats(self, session: aiohttp.ClientSession, match_id):
         """根据 match_id 获取比赛数据"""
         get_url = f"https://gate.5eplay.com/crane/http/api/data/match/{match_id}"
@@ -144,7 +151,7 @@ class Cstatscheck(Star):
             data = await resp.json()
             return data.get("data", {})
 
-    async def process_json(self, json_data, match_id, player_send):
+    async def process_json(self, json_data, match_id, player_send, teammate_of_send: list[str] | None = None):
         """处理比赛数据，提取并格式化战绩信息"""
         # 先存一份完整 json 数据留作备份
         with open(self.data_dir / f"match_{match_id}.json", "w", encoding="utf-8") as f:
@@ -155,21 +162,30 @@ class Cstatscheck(Star):
         group_1 = json_data.get("group_1", [])
         group_2 = json_data.get("group_2", [])
         players = group_1 + group_2
+        players_to_find = [player_send] + (teammate_of_send if teammate_of_send else [])
+        players = [p for p in players if p.get("user_info", {}).get("user_data", {}).get("username") in players_to_find]
         player_data = {}
         for player in players:
+            is_win = "胜利" if player.get("fight", {}).get("is_win") == 1 else "失败"
             adr = player.get("fight", {}).get("adr")
             rating = player.get("fight", {}).get("rating2")
             change_elo = player.get("sts", {}).get("change_elo")
             playername = player.get("user_info", {}).get("user_data", {}).get("username")
             # 创建字典数组存储信息
             player_data[playername] = {
+                "is_win": is_win,
                 "adr": adr,
                 "rating": rating,
                 "change_elo": change_elo
             }
-            # 存进 json 文件查看
-            with open(self.data_dir / f"match_{match_id}_players.json", "w", encoding="utf-8") as f:
-                json.dump(player_data, f, ensure_ascii=False, indent=4)
+        # 存进 json 文件查看
+        with open(self.data_dir / f"match_{match_id}_players.json", "w", encoding="utf-8") as f:
+            json.dump(player_data, f, ensure_ascii=False, indent=4)
+        if len(players) == 1:
+            noobname, noobdata = "", {}
+        else:
+            noobname, noobdata = min(player_data.items(), key=lambda x: x[1]["rating"])
+        return match_map, player_data[player_send], noobname, noobdata
         # return map, player_data_list
         # 先测试性地简单返回一个 map
         return f"\n地图: {match_map}\nRating：{player_data[player_send]['rating']}\nADR：{player_data[player_send]['adr']}"
@@ -182,6 +198,7 @@ class Cstatscheck(Star):
         match = re.match(r"^(?:添加用户|用户|添加玩家|玩家)\s*(.+)", full_text)
         self.playername = match.groups()[0].strip()
         self.username = event.get_sender_name()
+        qq_id = event.get_sender_id()
         domain = None
         uuid = None
 
@@ -208,6 +225,7 @@ class Cstatscheck(Star):
             with open(self.user_data_file, "r", encoding="utf-8") as f:
                 player_data = json.load(f)
         player_data[self.username] = {
+            "qqid": qq_id,
             "name": self.playername,
             "domain": self.domain,
             "uuid": self.uuid,
@@ -232,6 +250,7 @@ class Cstatscheck(Star):
             return
         else:
             player_send = player_data[self.username]["name"]
+            qqid = player_data[self.username]["qqid"]
         player_info = player_data[self.username]
         # 更新 self.uuid
         self.uuid = player_info.get("uuid")
@@ -248,8 +267,32 @@ class Cstatscheck(Star):
                 return
             # else:
             #     yield event.plain_result(f"成功获取比赛 {match_id} 的详细数据。")
-            match_stats_json = await self.process_json(match_stats, match_id, player_send)
-            yield event.plain_result(f"{player_info.get('name')} 的最近一场比赛中：{match_stats_json}")
+            with open(self.user_data_file, "r", encoding="utf-8") as f:
+                player_data = json.load(f)
+            # 这里其实需要获取群聊中其他玩家的名字来加载 teammate_of_send
+            teammate_of_send = [pinfo["name"] for uname, pinfo in player_data.items() if uname != self.username]
+            match_map, player_send_data, noobname, noobdata = await self.process_json(match_stats, match_id, player_send, teammate_of_send)
+            player_send_text = f"5eplayer {player_send} 最近一场比赛战绩：\nMap: {match_map}\n比赛结果: {player_send_data['is_win']}\nRating: {player_send_data['rating']} \nADR: {player_send_data['adr']}\nElo变化: {player_send_data['change_elo']}\n"
+            if noobname == "":
+                noob_text = "你一个人偷偷玩的，真厉害"
+            elif noobname == player_send:
+                noob_text = "你就是最菜的，怎么敢查战绩的"
+            else:
+                noob_text = f"本场菜比是 {noobname}，打出了 {noobdata['rating']} 超高rating，这是人类啊"
+            final_text = player_send_text + noob_text
+            # yield event.chain_result([Plain(final_text)])
+            prov = self.context.get_using_provider(umo=event.unified_msg_origin)
+            if prov:
+                llm_resp = await prov.text_chat(
+                    prompt=f"{player_info.get('name')} 的最近一场比赛中：{final_text}",
+                    context=[
+                        {"role": "user", "content": ""},
+                        {"role": "cs战绩短评官", "content": "response balabala"}
+                    ],
+                    system_prompt="你需要对上面的cs战绩进行一个简短的评价，不超过二十个字，rating 和 adr 是这局表现的重要参考因素，rating 和 adr 很高(1.2raing以上或者90ADR以上，越高越强，夸得越凶)但是输了可以用悲情英雄、尽力局、拉满了、燃成灰了、一绿带四红来形容，打得好赢了可以用大哥、带飞等来形容，打得菜(0.8rating以下或者50ADR以下，越低越菜，骂得越狠)可以用美美隐身、这把睡了、摊子、fvv、玩家名称中选一部分+出(比如玩家名称叫玩机器，就可以称为 玩出) 作为称呼来形容，如果战绩一般就正常评价吧"
+                )
+                logger.info(llm_resp)
+                yield event.chain_result([Plain(player_send_text), Plain(llm_resp.completion_text)])
 
     async def user_is_added(self, username: str, playername: str) -> bool:
         """检查用户是否已添加玩家数据"""
